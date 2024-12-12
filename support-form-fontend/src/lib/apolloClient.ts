1072ed997dca28a41ccc859d697b5277417be206
VonCauli <34951209+VonCauli@users.noreply.github.com>
2024-12-11 21:02:02 -0500

    Update apolloClient.ts

--- support-form-fontend/src/lib/apolloClient.ts
+++ support-form-fontend/src/lib/apolloClient.ts
@@ -1,11 +1,9 @@
-// lib/apolloClient.tsx
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 import { createUploadLink } from 'apollo-upload-client';
 
 // Create an upload link using apollo-upload-client
 const uploadLink = createUploadLink({
-  uri: 'https://api.plain.com/graphql', // Replace with your actual GraphQL endpoint
-  // You can add additional options here, such as headers
+  uri: 'https://api.plain.com/graphql', // Replace with actual GraphQL endpoint
   headers: {
     // Example: Authorization header
     // Authorization: `Bearer ${yourAuthToken}`,
